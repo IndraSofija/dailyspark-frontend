db7934ddb29ee1f23e376c542a9b9735c6c93fc3
IndraSofija <dogadova@gmail.com>
2025-05-10 20:50:51 +0400

    Create cancel.tsx

---
+++ pages/cancel.tsx
@@ -0,0 +1,9 @@
+export default function CancelPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>❌ Maksājums tika pārtraukts</h1>
+      <p>Vēlies mēģināt vēlreiz?</p>
+      <a href="/">Atgriezties sākumlapā</a>
+    </div>
+  );
+}
