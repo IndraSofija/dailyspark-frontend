3cb940d5113abac738143fc588da799e88f9e0db
IndraSofija <dogadova@gmail.com>
2025-05-10 20:49:46 +0400

    Create success.tsx

---
+++ pages/success.tsx
@@ -0,0 +1,8 @@
+export default function SuccessPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🎉 Tavs Pro līmenis ir aktivizēts!</h1>
+      <p>Paldies, ka izvēlējies DailySpark Pro!</p>
+    </div>
+  );
+}
